d4fab4edf53a4d92225907491a8943e33f261747
geliangchen <katsu119@foxmail.com>
2023-02-22 14:02:04 +0800

    add lsp for verilog

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,7 +59,7 @@ cmp.setup({
       "s",
     }),
   }),
-  
+
   -- 这里重要
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,10 @@ require("mason-lspconfig").setup ({
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+require("lspconfig").verible.setup {
+  capabilities = capabilities,
+}
+
 require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
 }
